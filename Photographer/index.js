df0f2ae0ac464f8fbdcdf27d44540462072532e3
Tate Brasel <tate@Shuksan.domain>
2018-06-26 09:36:23 -0700

    Added comments to Photographer

--- Photographer/index.js
+++ Photographer/index.js
@@ -1,60 +1,78 @@
 (function() {
 	"use strict";
 
+	/**
+	 * Give functionality to the search and clear buttons.
+	 */
 	window.onload = function() {
-		$("search-button").onclick = function() {
-			callAjax();
-		}
-		$("clear").onclick = function() {
-			let gallery = document.querySelector("section");
-			gallery.innerHTML = "";
-			$("search-item").value = "";
+		$("search-button").onclick = imageSearch;
+		$("clear-button").onclick = function() {
+			$("gallery").innerHTML = "";
+			$("search-bar").value = "";
 		}
 	};
 
-	function callAjax() {
-		let gallery = document.querySelector("section");
+	/**
+	 * Fetches image data from the server based on search term.
+	 */
+	function imageSearch() {
+		let gallery = $("gallery");
 		gallery.innerHTML = "";
 
+		// Show loading text
 		let loadingCard = document.createElement("div");
         let loadingText = document.createElement("h2");
         loadingText.innerText = "Loading images...";
         loadingCard.appendChild(loadingText);
         gallery.appendChild(loadingCard);
 
-		let query = "search=" + $("search-item").value;
+        // Generate url with query
+		let query = "search=" + $("search-bar").value;
 		let url = "http://localhost:3000/?" + query;
+
 	    fetch(url)
 	       	.then(checkStatus)
 	       	.then(JSON.parse)
-	       	.then(function(response) {
-	            gallery.innerHTML = "";
-
-	            
-
-	            for (let i = 0; i < response.images.length; i++) {
-	            	let imageCard = document.createElement("div");
-	            	imageCard.classList.add("shadow");
-	            	imageCard.classList.add("text-center");
-	            	let image = document.createElement("img");
-	            	image.src = response.images[i].url;
-
-	            	let desc = document.createElement("p");
-	            	let a = response.images[i].url.substr(response.images[i].url.indexOf(":") + 3, response.images[i].url.length - response.images[i].url.indexOf(":") + 3);
-					desc.innerText = a.substr(0, a.indexOf("/"));
-
-	            	imageCard.appendChild(image);
-	            	imageCard.appendChild(desc);
-	            	gallery.appendChild(imageCard);
-	            }
-	        })
+	       	.then(populateGallery)
 	       	.catch(function(error) {
-	        	console.log("There was an error: " + error);
+	        	console.log(error);
 	       	});
 	}
 
+	/**
+	 * Populate the gallery section with the images from a server request, along with the website they came from.
+	 * @param response - The server fetch response as a JSON object.
+	 */
+	function populateGallery(response) {
+		gallery.innerHTML = "";
+
+        for (let i = 0; i < response.images.length; i++) {
+        	// Show image in a stylized card
+        	let imageCard = document.createElement("div");
+        	imageCard.classList.add("shadow");
+        	imageCard.classList.add("text-center");
+        	let image = document.createElement("img");
+        	image.src = response.images[i].url;
+
+        	// Show source website title
+        	let titleObj = document.createElement("p");
+        	let url = response.images[i].url;
+        	let titleStart = url.indexOf(":") + 3;
+        	let title = url.substr(titleStart, url.length - titleStart);
+			titleObj.innerText = title.substr(0, title.indexOf("/"));
+
+        	imageCard.appendChild(image);
+        	imageCard.appendChild(titleObj);
+        	gallery.appendChild(imageCard);
+        }
+	}
+
+	/**
+	 * Checks the status of an AJAX fetch request.
+	 * @param response - The fetch response.
+	 * @return either the contents of a successful response or a rejected Promise
+	 */
 	function checkStatus(response) {
-		console.log(response);
 	    if (response.status >= 200 && response.status < 300) {
 	        return response.text();
 	    } else {
@@ -62,6 +80,11 @@
 	    }
 	}
 
+	/**
+	 * Finds an element by its id.
+	 * @param String id - The id of the element.
+	 * Return the document element.
+	 */
 	function $(id) {
 		return document.getElementById(id);
 	}
--- Photographer/server.js
+++ Photographer/server.js
@@ -1,16 +1,18 @@
 // Uses the g-i-s Google Image Seach module by Jim Kang (https://www.npmjs.com/package/g-i-s)
 
+// Import modules
 var imageSearch = require('g-i-s');
 var express = require('express');
 var app = express();
 
-// Allows CORS so we can recieve fetch reqests to localhost
+// Enable Cross-Origin Resource Sharing (CORS) to recieve reqests to localhost. Read more at: https://en.wikipedia.org/wiki/Cross-origin_resource_sharing
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
+// When a get request is made, search for the request query item and return all the resulting image data.
 app.get('/', function(req, res, next) {
 	imageSearch(req.query.search, function(err, results) {
 		res.set('Content-Type', 'application/json');
@@ -18,6 +20,7 @@ app.get('/', function(req, res, next) {
 	});
 });
 
+// Listen on port 3000 for requests from clients
 app.listen(3000, function() {
 	console.log('Example app listening on port 3000!');
 });
