f2143862017b1a93c026c58509dae747ecaf21f9
Tate Brasel <tate@Shuksan.domain>
2018-06-27 14:26:39 -0700

    Added archiving to Translator and fixed initial language selector

--- Translator/app.js
+++ Translator/app.js
@@ -5,8 +5,14 @@
 
 	app.controller('ctr', function($scope, $http) {
 		
+		$scope.languages = [{name: "Spanish", code: "es"},
+							{name: "French", code: "fr"},
+							{name: "Japanese", code: "ja"}];
+		$scope.to = $scope.languages[0].code;
+
 		$scope.translation = "(No translation)";
 		$scope.errors = "";
+		$scope.archive = [];
 
 		$scope.translate = function() {
 			let url = "http://localhost:3000/?to=" + $scope.to + "&phrase=" + $scope.phrase;
@@ -18,6 +24,16 @@
 			        $scope.errors = response.data.from.text.value;
 			    });
 		};
+
+
+		$scope.archiveEntry = function() {
+			$scope.archive.unshift({input: $scope.phrase, output: $scope.translation});
+		};
+
+		$scope.clearArchive = function() {
+			$scope.archive = [];
+		}
+		
 	});
 
 })();
