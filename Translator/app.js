3a33e849ef91a9be48de343acb71c52ec80f6df7
Tate Brasel <tate@Shuksan.domain>
2018-07-03 12:28:28 -0700

    Formatted the Translator app to follow John Papa's style guide

--- Translator/app.js
+++ Translator/app.js
@@ -1,56 +1,6 @@
 (function() {
-	'use strict';
+	"use strict";
 
 	// Define the Mr. Worldwide application module
-	var app = angular.module('app', []);
-
-	// Define the main controller for the application
-	app.controller('ctr', function($scope, $http) {
-		// Define all translatable languages and their codes
-		$scope.languages = [{name: "English", code: "en"},
-							{name: "Spanish", code: "es"},
-							{name: "French", code: "fr"},
-							{name: "German", code: "de"},
-							{name: "Italian", code: "it"},
-							{name: "Swedish", code: "sv"},
-							{name: "Japanese", code: "ja"}];
-
-		// Set initial source and target languages
-		$scope.sourceLanguage = $scope.languages[0].code;
-		$scope.targetLanguage = $scope.languages[1].code;
-
-		$scope.translation = "";
-		$scope.archive = [];
-
-		// Translates the current phrase and displays its translation
-		$scope.translate = function() {
-			let url = "http://localhost:3000/?sourcelanguage=" + $scope.sourceLanguage + "&targetlanguage=" + $scope.targetLanguage + "&phrase=" + $scope.phrase;
-			$http.get(url)
-			    .then(function(response) {
-			        $scope.translation = response.data.text;
-			    }).catch(function(err) {
-			    	console.log("Error: " + err);
-			    });
-		};
-
-		// Enter the current phrase and its translation into the archive
-		$scope.archiveEntry = function() {
-			$scope.archive.unshift({input: "[" + $scope.sourceLanguage.toUpperCase() + "]: " + $scope.phrase, output: "[" + $scope.targetLanguage.toUpperCase() + "]: " + $scope.translation});
-		};
-
-		// Empty all the contents of the archive
-		$scope.clearArchive = function() {
-			$scope.archive = [];
-		}
-
-		// Swaps the source and target languages as well as the phrase
-		$scope.swapLanguages = function() {
-			let temp = $scope.sourceLanguage;
-			$scope.sourceLanguage = $scope.targetLanguage;
-			$scope.targetLanguage = temp;
-			
-			$scope.phrase = $scope.translation;
-			$scope.translate();
-		}
-	});
+	angular.module("app", []);
 })();
---
+++ Translator/app_translator.js
@@ -0,0 +1,61 @@
+(function() {
+	"use strict";
+
+	angular.module("app")
+		.controller("TranslateController", translateController);
+
+	function translateController($scope, $http) {
+		var vm = this;
+
+		///////// BINDABLE MEMBERS ////////////
+		vm.archive = [];
+		vm.archiveEntry = archiveEntry;
+		vm.clearArchive = clearArchive;
+		vm.languages = [{name: "English", 	code: "en"},
+						{name: "Spanish", 	code: "es"},
+					 	{name: "French", 	code: "fr"},
+						{name: "German", 	code: "de"},
+						{name: "Italian", 	code: "it"},
+						{name: "Swedish", 	code: "sv"},
+						{name: "Japanese", 	code: "ja"}];
+		vm.sourceLanguage = vm.languages[0].code;
+		vm.swapLanguages = swapLanguages;
+		vm.targetLanguage = vm.languages[1].code;
+		vm.translation = "";
+		vm.translate = translate;
+
+		///////// METHOD IMPLEMENTATION //////////
+
+		// Enter the current phrase and its translation into the archive
+		function archiveEntry() {
+			vm.archive.unshift({ input: "[" + vm.sourceLanguage.toUpperCase() + "]: " + vm.phrase, 
+							 	 output: "[" + vm.targetLanguage.toUpperCase() + "]: " + vm.translation });
+		}
+
+		// Empty all the contents of the archive
+		function clearArchive() {
+			vm.archive = [];
+		}
+
+		// Swaps the source and target languages as well as the phrase
+		function swapLanguages() {
+			let temp = vm.sourceLanguage;
+			vm.sourceLanguage = vm.targetLanguage;
+			vm.targetLanguage = temp;
+			vm.phrase = vm.translation;
+			vm.translate();
+		}
+
+		// Translates the current phrase and displays its translation
+		function translate() {
+			let url = "http://localhost:3000/?sourcelanguage=" + vm.sourceLanguage + "&targetlanguage=" + vm.targetLanguage + "&phrase=" + vm.phrase;
+			$http.get(url)
+			    .then(function(response) {
+			        vm.translation = response.data.text;
+			    }).catch(function(err) {
+			    	console.log(err);
+			    });
+		}
+	}
+
+})();
